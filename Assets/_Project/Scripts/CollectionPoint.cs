812021b89d6c0dbe630eb0d98e02105485e220b7
Saryos <tosiear@gmail.com>
2025-02-23 14:21:47 +0200

    collection meter

--- Assets/_Project/Scripts/CollectionPoint.cs
+++ Assets/_Project/Scripts/CollectionPoint.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class CollectionPoint : MonoBehaviour
 {
+    [SerializeField] private int collectionGoal = 10;
+    public Sprite collectionMeter;
+    //private GameObject collectionMeter;
+
+    private Vector3 collectionMeterOriginal;
+
     public static int collected = 0;
 
     public static int getCollected()
@@ -23,6 +29,20 @@ public class CollectionPoint : MonoBehaviour
 
             playerController.ReleasePrisoner();
             collected++;
+        }
+    }
+
+    private void Start(){
+        //collectionMeter = GameObject.Find("CollectionMeter").GetComponent();
+        collectionMeterOriginal = transform.Find("CollectionMeter").position;
+    }
+
+    private void Update()
+    {
+        transform.Find("CollectionMeter").localScale = new Vector3(1, 0.1f*collected, 1);
+        transform.Find("CollectionMeter").position = collectionMeterOriginal + new Vector3(0, -1 + 0.05f*collected, 0);
+        if (collected >= collectionGoal){
+            // victory
         }
     }
 }
